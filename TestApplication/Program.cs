d4a046117512ce71160a0772aa3e4864a4f63c29
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-12-30 22:38:17 +0100

    cleanup

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -1,31 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using NLog;
 
 namespace TestApplication
 {
     static class Program
     {
-
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-       
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-
-
         }
     }
 }
