8fe7911ee3a32a7d851b887651ee3e4606de7aa3
Julian Verdurmen <304NotModified@users.noreply.github.com>
2020-12-30 22:53:17 +0100

    cleanup

--- TestApplicationCore3/Form1.cs
+++ TestApplicationCore3/Form1.cs
@@ -24,14 +24,6 @@ namespace TestApplicationCore3
                 for (int i = 0; i < 10; i++)
                 {
                     LogEventInfo theEvent = new LogEventInfo(LogLevel.Debug, "", i + ": a line with some length\n a new line");
-                    if (rnd.NextDouble() > 0.1)
-                    {
-                        theEvent.Properties["ShowLink"] = "link via property";
-                    }
-                    if (rnd.NextDouble() > 0.5)
-                    {
-                        theEvent.Properties["ShowLink2"] = "Another link";
-                    }
                     Logger.Log(theEvent);
                     Thread.Sleep(200);
                 }
@@ -44,25 +36,6 @@ namespace TestApplicationCore3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            RichTextBoxTarget.GetTargetByControl(richTextBox1).LinkClicked += Form1_LinkClicked;
-            RichTextBoxTarget.GetTargetByControl(richTextBox2).LinkClicked += Form1_LinkClicked;
-        }
-
-        private void Form1_LinkClicked(RichTextBoxTarget sender, string linkText, LogEventInfo logEvent)
-        {
-            //COM HRESULT E_FAIL happens when not used BeginInvoke and links are clicked while spinning
-            BeginInvokeLambda(this,
-                () => { MessageBox.Show("Clicked link '" + linkText + "' for event\n" + logEvent, sender.Name); }
-            );
-        }
-
-        private static IAsyncResult BeginInvokeLambda(Control control, Action action)
-        {
-            if (!control.IsDisposed)
-            {
-                return control.BeginInvoke(action, null);
-            }
-            return null;
         }
     }
 }
