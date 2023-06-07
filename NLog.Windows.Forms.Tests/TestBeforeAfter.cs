9cef81cd647af0cc630c27f2d28c6ca9a81cca80
Rolf Kristensen <sweaty1@hotmail.com>
2023-06-07 21:56:03 +0200

    Updated to NLog v5.2 with RegisterWindowsForms

---
+++ NLog.Windows.Forms.Tests/TestBeforeAfter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace NLog.Windows.Forms.Tests
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class LogManagerResetAttribute : BeforeAfterTestAttribute
+    {
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            LogManager.Shutdown();
+            LogManager.ThrowExceptions = true;
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            LogManager.Shutdown();
+        }
+    }
+}
---
+++ NLog.Windows.Forms/Config/SetupBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using NLog.Config;
+
+namespace NLog
+{
+    /// <summary>
+    /// Extension methods to setup LogFactory options
+    /// </summary>
+    public static class SetupBuilderExtensions
+    {
+        /// <summary>
+        /// Register the NLog.Windows.Forms extensions before loading NLog config
+        /// </summary>
+        public static ISetupBuilder RegisterWindowsForms(this ISetupBuilder setupBuilder)
+        {
+            setupBuilder.SetupExtensions(e => e.RegisterWindowsForms());
+            return setupBuilder;
+        }
+    }
+}
