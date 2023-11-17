ff48f4170cca1d99b49242e7db624b9a707d2055
Rolf Kristensen <sweaty1@hotmail.com>
2023-11-17 20:03:05 +0100

    Added mark as IsTrimmable

---
+++ NLog.Windows.Forms/AssemblyMetadataAttribute.cs
@@ -0,0 +1,12 @@
+﻿#if NET35
+namespace System.Reflection
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    internal sealed class AssemblyMetadataAttribute : Attribute
+    {
+        public AssemblyMetadataAttribute(string key, string value)
+        {
+        }
+    }
+}
+#endif
